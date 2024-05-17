35dfbba6f7973aa8d0a235059796af71c4eb7cd4
BillysugarcaneRoy <mailelroi99@gmail.com>
2024-05-17 19:32:18 +0100

    next responsive

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -6,8 +6,32 @@ import location_icon from '../../assets/location_icon.svg'
 import call_icon from '../../assets/call_icon.svg'
 
 const Contact = () => {
+
+   const onSubmit = async (event) => {
+      event.preventDefault();
+      const formData = new FormData(event.target);
+
+      formData.append('access_key', 'YOUR_ACCESS_KEY_HERE');
+
+      const object = Object.fromEntries(formData);
+      const json = JSON.stringify(object);
+
+      const res = await fetch('https://api.web3forms.com/submit', {
+         method: 'POST',
+         headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json'
+         },
+         body: json
+      }).then((res) => res.json());
+
+      if (res.success) {
+         console.log('Success', res);
+      }
+   };
+
    return (
-      <div className='contact'>
+      <div id='contact' className='contact'>
          <div className="contact-title">
             <h1>Get in touch</h1>
             <img src={theme_pattern} alt="" />
